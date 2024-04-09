0320891851537de2e17ed65a69c8a036d408c8e6
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-09 14:13:20 +0200

    GET

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,20 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
+console.log(contactsPath);
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +22,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
