556f53c8ff788c29f36e45cccdb3f5659e4193a9
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-12 18:09:00 +0200

    POST+ DELETE

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,7 @@
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 const path = require("path");
+const ID = nanoid();
 
 const contactsPath = path.join(__dirname, "contacts.json");
 console.log(contactsPath);
@@ -13,9 +15,25 @@ const getContactById = async (contactId) => {
   return contacts.find(({ id }) => id === contactId);
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(({ id }) => id === contactId);
+  if (index !== -1) {
+    contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return true;
+  }
+  return false;
+};
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const { name, email, phone } = body;
+  const newContact = { ID, name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
 const updateContact = async (contactId, body) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -40,11 +40,26 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    return res.status(400).json({ message: "missing required field" });
+  }
+  const newContact = await contacts.addContact(req.body);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: newContact,
+  });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const isDeleted = await contacts.removeContact(contactId);
+  if (isDeleted) {
+    res.status(200).json({ message: "contact deleted" });
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
