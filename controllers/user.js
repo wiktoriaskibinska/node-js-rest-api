f731252f03070e2bee11c5620d6b53a90829d5f4
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-26 17:04:10 +0200

    .

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,33 @@
 const User = require("../models/userSchema");
 
-const login = async (req, res, next) => {};
+const register = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ email }).lean();
+  if (user) {
+    return res.status(409).json({
+      status: "error",
+      code: 409,
+      message: "Email is already in use",
+      data: "Conflict",
+    });
+  }
+  try {
+    const newUser = new User({ username, email });
+    newUser.setPassword(password);
+    await newUser.save();
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        message: "Registration successful",
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const register = async (req, res, next) => {};
+const login = async (req, res, next) => {};
 
 const logout = async (req, res, next) => {};
 
