80fa63dab9b5b1d1ef8fe2a8ad13991d8dad2aa1
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-26 16:56:20 +0200

    token auth

--- controllers/auth.js
+++
@@ -1,8 +0,0 @@
-const User = require("../models/userSchema");
-//fukcja sprawdająca token import
-const auth = async (req, res, next) => {
-  /**przyjmuje w req token ktory jest sprawdzany a nastepnie po sprawdzenie
-   * sprawie ze req.user to dane uzytkownika z tokena
-   * jeslo się nie powiedzie to nalezu zwrocic res unathorized user
-   */
-};
---
+++ controllers/user.js
@@ -0,0 +1,9 @@
+const User = require("../models/userSchema");
+
+const login = async (req, res, next) => {};
+
+const register = async (req, res, next) => {};
+
+const logout = async (req, res, next) => {};
+
+const current = async (req, res, next) => {};
